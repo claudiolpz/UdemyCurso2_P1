32ca3723c5cac2d4a1bad65435152a4403d0b732
claudiolpz <claudio.lopez1@mail.udp.cl>
2025-04-03 17:21:06 -0300

    Inicio Proyecto

---
+++ proyecto_rect/vite/ejemplo_vite/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  const ejemplo = <h1>Hola desde JSX INTERPOLACION</h1>
+  return (
+    <div className="App">
+      <h1>Hola Mundo</h1>
+      {ejemplo}
+    </div>
+  )
+}
+
+export default App
