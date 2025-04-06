d867daf328b9b06876dd92cda6bdc6fb4f0b1962
claudiolpz <claudio.lopez1@mail.udp.cl>
2025-04-06 18:40:46 -0400

    Style components

--- proyecto_rect/vite/ejemplo_vite/src/App.jsx
+++ proyecto_rect/vite/ejemplo_vite/src/App.jsx
@@ -1,13 +1,17 @@
 import ComponenteDeClase from "./components/ComponenteDeClase";
 import ComponenteFuncional from "./components/ComponenteFuncional";
 import ComponenteFuncional1v2 from "./components/ej/ComponenteFuncional1v2";
+import StyleComponent from "./components/StyleComponent";
+
 
 function App() {
   
   return (
-    
-      <ComponenteFuncional/> 
-     
+    <>
+    {/* <ComponenteFuncional /> */}
+
+    <StyleComponent />
+    </>   
   );
 }
 
--- proyecto_rect/vite/ejemplo_vite/src/components/ComponenteFuncional2.jsx
+++ proyecto_rect/vite/ejemplo_vite/src/components/ComponenteFuncional2.jsx
@@ -1,53 +1,45 @@
- import Pais from "./Pais"
+import Pais from "./Pais";
 
-    const ComponenteFuncional2 = ({prop1, nombre, paises}) => {
-    return (
-        <>
-      
-          <h3>desde componente2 funcional</h3>
-      
+const ComponenteFuncional2 = ({ prop1, nombre, paises }) => {
+  return (
+    <>
+      <h3>desde componente2 funcional</h3>
+
       <ul>
         <li>Prop1: {prop1}</li>
         <li>Nombre: {nombre}</li>
         <li>
-            Paises:
-            <ul>
-                {paises.map((pais) => (
-                <li key={pais.id}>
-                    {pais.nombre}
-                </li>
-                ))}
-            </ul>
+          Paises:
+          <ul>
+            {paises.map((pais) => (
+              <li key={pais.id}>{pais.nombre}</li>
+            ))}
+          </ul>
         </li>
       </ul>
       <hr />
       <table>
         <thead>
-
-       
-        <tr>
+          <tr>
             <th>ID</th>
             <th>Nombre</th>
             <th>Capital</th>
-        </tr>
+          </tr>
         </thead>
         <tbody>
-            {paises.map((pais) => (
-              pais.nombre == "Chile" ? (
-                <Pais key={pais.id} pais={pais}/>
-              ):null
+          {paises.map((pais) =>
+            pais.nombre == "Chile" ? <Pais key={pais.id} pais={pais} /> : null
+          )}
 
-            ))}
-
-            {paises.map((pais) => (
-              pais.nombre == "Argentina" &&  <Pais key={pais.id} pais={pais}/>
-            ))}
+          {paises.map(
+            (pais) =>
+              pais.nombre == "Argentina" && <Pais key={pais.id} pais={pais} />
+          )}
         </tbody>
-
       </table>
-      </>
-    )
-  }
-  
-  export default ComponenteFuncional2
-  
+    </>
+  );
+};
+
+
+export default ComponenteFuncional2;
