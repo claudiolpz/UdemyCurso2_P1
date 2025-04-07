bc120f7397adae436934b89ca172ef3e31abeca7
claudiolpz <claudio.lopez1@mail.udp.cl>
2025-04-07 13:30:53 -0400

    Boostrap Layout

---
+++ proyecto_rect/vite/ejemplo_vite/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="py-5 text-center text-body-secondary bg-body-tertiary">
+  <p>Desarrollado por Claudio Lopez <a href="https://getbootstrap.com/">LOLOLOL</a> by <a href="https://x.com" target='_blank' title='OOLO'>OOLO</a>.</p>
+  <p className="mb-0">
+    <a href="#">Back to top</a>
+  </p>
+</footer>
+  )
+}
+
+export default Footer
