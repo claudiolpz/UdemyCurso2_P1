ef05ff75fe9fcac86962913ba789fe22c36e2e08
claudiolpz <claudio.lopez1@mail.udp.cl>
2025-04-29 14:37:39 -0400

    useContext para rutas protegidas

--- proyecto_rect/vite/ejemplo-vite2-tailwindcss/src/context/AuthProvider.jsx
+++ proyecto_rect/vite/ejemplo-vite2-tailwindcss/src/context/AuthProvider.jsx
@@ -1,23 +1,32 @@
 import { createContext, useState } from "react";
 
 
-const AuthContext = createContext()
+const AuthContext = createContext();
 
-const AuthProvider = ({children}) => {
-    const [auth, setAuth] = useState((localStorage.getItem('tokenTamila') != null) ? true : false);
-
-    const handleIniciarSesion=(t, nombre) => {
-        localStorage.setItem('tokenTamila', t);
-        localStorage.setItem('tokenNombre', nombre);
-        setAuth(true);
+const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState(
+    localStorage.getItem("tokenTamila") != null ? true : false
+  );
+  const handleEstaLogeado = () =>{
+    if(!auth && localStorage.getItem("tokenTamila") == null){
+      window.location='/acceso/login';
     }
-    return(
-        <AuthContext.Provider value={{
-            auth, handleIniciarSesion
-        }}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
-export {AuthProvider}
-export default AuthContext
+  }
+  const handleIniciarSesion = (t, nombre) => {
+    localStorage.setItem("tokenTamila", t);
+    localStorage.setItem("tokenNombre", nombre);
+    setAuth(true);
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        auth,
+        handleIniciarSesion,handleEstaLogeado,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+export { AuthProvider };
+export default AuthContext;
--- proyecto_rect/vite/ejemplo-vite2-tailwindcss/src/paginas/AccesoProtegido.jsx
+++ proyecto_rect/vite/ejemplo-vite2-tailwindcss/src/paginas/AccesoProtegido.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import {useContext, useEffect} from 'react'
+import AuthContext from '../context/AuthProvider'
 
 const AccesoProtegido = () => {
+    const {handleEstaLogeado} = useContext(AuthContext)
+    useEffect(() => {
+
+        return()=>{
+        handleEstaLogeado()
+        }
+    }, [])
   return (
     <div>
-      
+      <h1>Protegido</h1>
     </div>
   )
 }
