0b8b0d322d59210791f54b3215bd2858591a65fb
claudiolpz <claudio.lopez1@mail.udp.cl>
2025-05-05 13:44:51 -0400

    .Rar proyecto anterior

--- proyecto_rect/vite/ejemplo-vite2-tailwindcss/src/validaciones/index.jsx
+++
@@ -1,2 +0,0 @@
-export const expresion_correo = new RegExp("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$");
-export const expresion_password = new RegExp("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,20}$");
