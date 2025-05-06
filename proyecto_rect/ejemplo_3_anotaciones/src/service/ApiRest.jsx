019021c1f40f9148ea2fe19d0725041d5502fe0d
claudiolpz <claudio.lopez1@mail.udp.cl>
2025-05-06 14:17:37 -0400

    API REST

---
+++ proyecto_rect/ejemplo_3_anotaciones/src/service/ApiRest.jsx
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+let bearer='';
+
+if(localStorage.getItem('tokenTamilaAnotacion')!=null){
+    bearer=localStorage.getItem('tokenTamilaAnotacion');
+}else{
+    let datos = authLogin({correo: import.meta.env.VITE_API_CORREO, password: import.meta.env.VITE_API_PASSWORD});
+    bearer=localStorage.getItem('tokenTamilaAnotacion');
+}
+let cabeceros = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer' + bearer
+}
+export async function authLogin(request){
+    let datos = axios.post(`${import.meta.env.VITE_API_URL_LOCAL}login`, request, {
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    }).then((response) => {
+        localStorage.setItem('tokenTamilaAnotacion', response.data.token);
+        return;
+    }).catch((error) => {
+        return {"estado": "error"}
+    })
+    return datos;
+}
