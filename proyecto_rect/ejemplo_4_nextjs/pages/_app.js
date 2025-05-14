5854c3ec4b53907ef6e8a232789080c2c6a18450
claudiolpz <claudio.lopez1@mail.udp.cl>
2025-05-13 23:46:50 -0400

    context y cookies

--- proyecto_rect/ejemplo_4_nextjs/pages/_app.js
+++ proyecto_rect/ejemplo_4_nextjs/pages/_app.js
@@ -1,5 +1,21 @@
 import "@/styles/globals.css";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Swal from "sweetalert2";
+import nookies, {setCookies, destroyCookie} from 'nookies'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter()
+  const [auth, setAuth]=useState(false)
+  const [authNombre,setAuthNombre] = ('')
+  const [authCorreo,setAuthCorreo] = ('')
+  
+  return <Component {...pageProps}
+  auth={auth}
+  setAut={setAuth}
+  authNombre={authNombre}
+  authCorreo={authCorreo}
+  setAuthNombre={setAuthNombre}
+  setAuthCorreo={setAuthCorreo}
+  />;
 }
