ecfc47da4b6d1a0f7b2e6061e57fa842090df6f1
claudiolpz <claudio.lopez1@mail.udp.cl>
2025-05-15 22:11:35 -0400

    Crear - Editar - Eliminar Categorias

--- proyecto_rect/ejemplo_4_nextjs/services/ApiRest.js
+++ proyecto_rect/ejemplo_4_nextjs/services/ApiRest.js
@@ -1,158 +1,230 @@
 import axios from "axios";
 
 let cabeceros = {
-    'content-type': 'application/json'
-}
+  "content-type": "application/json",
+};
 
 let cabeceros_upload = {
-    'content-type': 'multipart/form-data'
-}
+  "content-type": "multipart/form-data",
+};
 
 export async function getCategorias() {
-
-    let datos = await axios.get(`${process.env.NEXT_PUBLIC_API_URL_LOCAL}clasificados-categorias`, {headers:cabeceros
-
-    }).then((response)=>{
-        return response.data
+  let datos = await axios
+    .get(`${process.env.NEXT_PUBLIC_API_URL_LOCAL}clasificados-categorias`, {
+      headers: cabeceros,
     })
-    .catch((err)=>{
-        console.log("error: "+err)
+    .then((response) => {
+      return response.data;
     })
-    return datos
+    .catch((err) => {
+      console.log("error: " + err);
+    });
+  return datos;
 }
 
 export async function getAvisos(page) {
-
-    let datos = await axios.get(`${process.env.NEXT_PUBLIC_API_URL_LOCAL}clasificados-avisos?page=${page}`, {headers:cabeceros
-
-    }).then((response)=>{
-        return response.data
+  let datos = await axios
+    .get(
+      `${process.env.NEXT_PUBLIC_API_URL_LOCAL}clasificados-avisos?page=${page}`,
+      { headers: cabeceros }
+    )
+    .then((response) => {
+      return response.data;
     })
-    .catch((err)=>{
-        console.log("error: "+err)
-    })
-    return datos
+    .catch((err) => {
+      console.log("error: " + err);
+    });
+  return datos;
 }
 export async function getAvisosPorId(id) {
-
-    let datos = await axios.get(`${process.env.NEXT_PUBLIC_API_URL_LOCAL}clasificados-avisos/${id}`, {headers:cabeceros
-
-    }).then((response)=>{
-        return response.data
+  let datos = await axios
+    .get(`${process.env.NEXT_PUBLIC_API_URL_LOCAL}clasificados-avisos/${id}`, {
+      headers: cabeceros,
     })
-    .catch((err)=>{
-        console.log("error: "+err)
+    .then((response) => {
+      return response.data;
     })
-    return datos
+    .catch((err) => {
+      console.log("error: " + err);
+    });
+  return datos;
 }
 
-export async function getAvisosPorCategoria(slug,page) {
-
-    let datos = await axios.get(`${process.env.NEXT_PUBLIC_API_URL_LOCAL}clasificados-avisos-categoria/${slug}?page=${page}`, {headers:cabeceros
-
-    }).then((response)=>{
-        return response.data
+export async function getAvisosPorCategoria(slug, page) {
+  let datos = await axios
+    .get(
+      `${process.env.NEXT_PUBLIC_API_URL_LOCAL}clasificados-avisos-categoria/${slug}?page=${page}`,
+      { headers: cabeceros }
+    )
+    .then((response) => {
+      return response.data;
     })
-    .catch((err)=>{
-        console.log("error: "+err)
-    })
-    return datos
+    .catch((err) => {
+      console.log("error: " + err);
+    });
+  return datos;
 }
 export async function getCategoriasPorSlug(slug) {
-
-    let datos = await axios.get(`${process.env.NEXT_PUBLIC_API_URL_LOCAL}clasificados-categorias/${slug}`, {headers:cabeceros
-
-    }).then((response)=>{
-        return response.data
+  let datos = await axios
+    .get(
+      `${process.env.NEXT_PUBLIC_API_URL_LOCAL}clasificados-categorias/${slug}`,
+      { headers: cabeceros }
+    )
+    .then((response) => {
+      return response.data;
     })
-    .catch((err)=>{
-        console.log("error: "+err)
-    })
-    return datos
+    .catch((err) => {
+      console.log("error: " + err);
+    });
+  return datos;
 }
 export async function getAvisosComentariosPorId(id) {
-
-    let datos = await axios.get(`${process.env.NEXT_PUBLIC_API_URL_LOCAL}clasificados-avisos-comentarios/${id}`, {headers:cabeceros
-
-    }).then((response)=>{
-        return response.data
+  let datos = await axios
+    .get(
+      `${process.env.NEXT_PUBLIC_API_URL_LOCAL}clasificados-avisos-comentarios/${id}`,
+      { headers: cabeceros }
+    )
+    .then((response) => {
+      return response.data;
     })
-    .catch((err)=>{
-        console.log("error: "+err)
-    })
-    return datos
+    .catch((err) => {
+      console.log("error: " + err);
+    });
+  return datos;
 }
 
 export async function addAvisosComentariosPorId(request) {
-
-    let datos = await axios.post(`${process.env.NEXT_PUBLIC_API_URL_LOCAL}clasificados-avisos-comentarios`, request, {headers:cabeceros
-
-    }).then((response)=>{
-        return response.status
+  let datos = await axios
+    .post(
+      `${process.env.NEXT_PUBLIC_API_URL_LOCAL}clasificados-avisos-comentarios`,
+      request,
+      { headers: cabeceros }
+    )
+    .then((response) => {
+      return response.status;
     })
-    .catch((err)=>{
-        console.log("error: "+err)
-    })
-    return datos
+    .catch((err) => {
+      console.log("error: " + err);
+    });
+  return datos;
 }
 export async function formularioContacto(request) {
-
-    let datos = await axios.post(`${process.env.NEXT_PUBLIC_API_URL_LOCAL}clasificados-contacto`, request, {headers:cabeceros
-
-    }).then((response)=>{
-        return response.status
+  let datos = await axios
+    .post(
+      `${process.env.NEXT_PUBLIC_API_URL_LOCAL}clasificados-contacto`,
+      request,
+      { headers: cabeceros }
+    )
+    .then((response) => {
+      return response.status;
     })
-    .catch((err)=>{
-        console.log("error: "+err)
-    })
-    return datos
+    .catch((err) => {
+      console.log("error: " + err);
+    });
+  return datos;
 }
 
 export async function getAvisosSearch(search) {
-
-    let datos = await axios.get(`${process.env.NEXT_PUBLIC_API_URL_LOCAL}clasificados-avisos-search?search=${search}`, {headers:cabeceros
-
-    }).then((response)=>{
-        return response.data
+  let datos = await axios
+    .get(
+      `${process.env.NEXT_PUBLIC_API_URL_LOCAL}clasificados-avisos-search?search=${search}`,
+      { headers: cabeceros }
+    )
+    .then((response) => {
+      return response.data;
     })
-    .catch((err)=>{
-        console.log("error: "+err)
-    })
-    return datos
+    .catch((err) => {
+      console.log("error: " + err);
+    });
+  return datos;
 }
 export async function authLogin(request) {
-
-    let datos = await axios.post(`${process.env.NEXT_PUBLIC_API_URL_LOCAL}login`, request,{
-        headers:cabeceros
-    }).then((response)=>{
-        return response.data
+  let datos = await axios
+    .post(`${process.env.NEXT_PUBLIC_API_URL_LOCAL}login`, request, {
+      headers: cabeceros,
     })
-    .catch((err)=>{
-        console.log("error: "+err)
+    .then((response) => {
+      return response.data;
     })
-    return datos
+    .catch((err) => {
+      console.log("error: " + err);
+    });
+  return datos;
 }
 export async function authRegistro(request) {
-
-    let datos = await axios.post(`${process.env.NEXT_PUBLIC_API_URL_LOCAL}registro`, request,{
-        headers:cabeceros
-    }).then((response)=>{
-        return response.data
+  let datos = await axios
+    .post(`${process.env.NEXT_PUBLIC_API_URL_LOCAL}registro`, request, {
+      headers: cabeceros,
     })
-    .catch((err)=>{
-        console.log("error: "+err)
+    .then((response) => {
+      return response.data;
     })
-    return datos
+    .catch((err) => {
+      console.log("error: " + err);
+    });
+  return datos;
 }
 export async function authMisDatos(request) {
-
-    let datos = await axios.post(`${process.env.NEXT_PUBLIC_API_URL_LOCAL}mis-datos`, request,{
-        headers:cabeceros
-    }).then((response)=>{ 
-        return response.status
+  let datos = await axios
+    .post(`${process.env.NEXT_PUBLIC_API_URL_LOCAL}mis-datos`, request, {
+      headers: cabeceros,
     })
-    .catch((err)=>{
-        console.log("error: "+err)
+    .then((response) => {
+      return response.status;
     })
-    return datos
+    .catch((err) => {
+      console.log("error: " + err);
+    });
+  return datos;
+}
+export async function addCategorias(request) {
+  let datos = await axios
+    .post(
+      `${process.env.NEXT_PUBLIC_API_URL_LOCAL}clasificados-categorias`,
+      request,
+      {
+        headers: cabeceros,
+      }
+    )
+    .then((response) => {
+      return response.status;
+    })
+    .catch((err) => {
+      console.log("error: " + err);
+    });
+  return datos;
+}
+export async function editCategorias(request, id) {
+  let datos = await axios
+    .put(
+      `${process.env.NEXT_PUBLIC_API_URL_LOCAL}clasificados-categorias/${id}`,
+      request,
+      {
+        headers: cabeceros,
+      }
+    )
+    .then((response) => {
+      return response.status;
+    })
+    .catch((err) => {
+      console.log("error: " + err);
+    });
+  return datos;
+}
+export async function deleteCategorias(id) {
+  let datos = await axios
+    .delete(
+      `${process.env.NEXT_PUBLIC_API_URL_LOCAL}clasificados-categorias/${id}`,
+      {
+        headers: cabeceros,
+      }
+    )
+    .then((response) => {
+      console.log(response.status);
+      return response.status;
+    })
+    .catch((err) => {
+      console.log("error: " + err);
+    });
+  return datos;
 }
